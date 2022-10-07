e8f9e994841c28bf4a813bceed1ab3a7b64c684e
Niles O'Neil <nilesoneil@gmail.com>
2022-10-07 16:46:28 -0700

    third test red fail

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -11,4 +11,8 @@ describe('calcAge', () => {
     const mercuryAge = new PersonAge(10);
     expect(mercuryAge.convertToMercury()).toEqual(2.4)
   });
+  test("It should take an age and convert to venus age", () => {
+    const venusAge = new PersonAge(10);
+    expect(venusAge.convertToVenus()).toEqual(6.2)
+  });
 });
