929faf41a2dfe490f47f0a6c18e4c6221efd08e9
Niles O'Neil <nilesoneil@gmail.com>
2022-10-07 16:21:20 -0700

    first test good fail

---
+++ __tests__/ageCalc.js
@@ -0,0 +1,8 @@
+describe('calcAge', () => {
+  test("It should take a user's age", () => {
+    //code
+    const personAge = new PersonAge(6);
+    //expect statements
+    expect(age).toEqual(6);
+  });
+});
